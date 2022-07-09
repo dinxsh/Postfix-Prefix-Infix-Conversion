4f96b9dd66ec9a7a678586de81a868a96b41b65a
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-09 18:51:13 +0530

    added tkinter bare code

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import tkinter
+from tkinter import *
 
-root = tkinter.Tk()
-root.geometry("800x800")
+root = Tk()
+root.geometry("600x600")
+root.title("Postfix Infix Prefix Conversion")
+
+explabel = Label(root, text="Expression")
+explabel.place(x=50, y=300)
+expinput = Text(root)
+explabel.place(x=50, y=320)
 
 root.mainloop()
