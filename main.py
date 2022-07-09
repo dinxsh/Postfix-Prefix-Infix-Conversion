37bc2ad223427a7564af0400d10c5279551355f5
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-09 19:04:29 +0530

    Added components

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import tkinter as tk
 from tkinter import *
 
 root = Tk()
-root.geometry("600x600")
+root.geometry("400x500")
 root.title("Postfix Infix Prefix Conversion")
 
-explabel = Label(root, text="Expression")
-explabel.place(x=50, y=300)
-expinput = Text(root)
-explabel.place(x=50, y=320)
+
+
+exp=tk.StringVar()
+
+explabel = Label(root, text="Expression", font=('Cambria', 11))
+explabel.place(x=50, y=100)
+expinput = tk.Entry(root, width="30")
+expinput.place(x=50, y=125)
 
 root.mainloop()
